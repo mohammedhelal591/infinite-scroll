ee705b6b8637c00369c793c6fd9c10c574a34ed7
MSA\saz.helal <mohamed.helal@sazience.com>
2025-07-22 16:22:29 +0300

    fixed import issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostListComponent } from './Components/post-list/post-list.component';
 
 @Component({
   selector: 'app-root',
